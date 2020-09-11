9ef7d9ba4012750e704b99eb67f29a0e00c0d3fc
coldkillerr <yonishitjain123456@gmail.com>
2020-09-11 16:18:04 +0530

    added case update file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,url_for,flash,redirect,session,sessions,abort
 import os
+import re
 from flask_sqlalchemy import SQLAlchemy
 from models import db ,Lawyer,Lawyer_case
 from  werkzeug.security import generate_password_hash , check_password_hash
@@ -61,6 +62,7 @@ def about():
     #<html code here>'''
 
 @app.route('/home')
+@login_required
 def home():
     user_cases = Lawyer_case.query.filter_by(lawyer_id=session['user_id'])
     return render_template('home.html',title='HOME',user_cases=user_cases)
@@ -87,7 +89,7 @@ def login():
 				login_user(user,remember = form.remember.data)
 				session['user_id'] = user.id
 				flash(f'Login successfull')
-				return render_template('layout.html')
+				return redirect(url_for('home'))
 						
 
 	
@@ -136,13 +138,55 @@ def cases():
 
 		flash(f'Case added for {form.Name.data}','success')
 		return redirect(url_for('home'))
-	return render_template('case.html',title='case',form=form)
+	return render_template('case.html',title='case',form=form , legend='NEW CASE')
 	
 
 @app.route('/case_display/<int:case_id>')
 def case_display(case_id):
 	case=Lawyer_case.query.get_or_404(case_id)
 	return render_template('case_display.html',case=case)
+
+@app.route('/case_display/<int:case_id>/update', methods = ['GET','POST'])
+@login_required
+def case_update(case_id):
+	case=Lawyer_case.query.get_or_404(case_id)
+	if case.lawyer_id != session['user_id']:
+		abort(403)
+	form=CaseForm()
+	form.Name.data = case.name
+	form.Day.data = case.day
+	form.Month.data = case.month
+	form.Year.data = case.year
+	form.Bio.data = case.bio
+	
+	# form.case_file.data = open(os.path.join(app.config['UPLOAD_FOLDER'],filename[1]),'r+')
+	if form.validate_on_submit():
+		case.name = form.Name.data
+		case.day = form.Day.data
+		case.month = form.Month.data
+		case.year = form.Year.data
+		case.bio = form.Bio.data
+		case_file = form.case_file.data
+		print(case_file)
+		if case_file:
+			case_file_name = secure_filename(case_file.filename)
+			if case_file_name != '':
+				file_ext = os.path.splitext(case_file_name)[1]
+				if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+					abort(400)
+				case_file.save(os.path.join(app.config['UPLOAD_FOLDER'], case_file_name))
+				case.case_file = url_for('static',filename='uploads/'+case_file_name)
+		# else:
+		# 	case.case_file = None
+		db.session.commit()
+		flash('Case Updated','success')
+	return render_template('case.html',title='UPDATE CASE',form=form,legend='UPDATE CASE')
+
+
+
+
+    
+ 
 
 
 #@app.route('/profile')
