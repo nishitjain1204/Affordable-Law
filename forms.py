9c1bbd19915c4055dbeeabf20924b5cb115cc8ff
coldkillerr <yonishitjain123456@gmail.com>
2020-09-12 18:03:49 +0530

    added new fields to profile updating form

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,IntegerField,BooleanField,FileField,FormField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo , URL
 
 class RegistrationForm(FlaskForm):
 	username= StringField('Username', validators=[DataRequired(),Length(min=2,max=25)])
@@ -13,22 +13,47 @@ class RegistrationForm(FlaskForm):
 class ProfileForm(FlaskForm):
 	FirstName= StringField('First Name', validators=[DataRequired(),Length(min=2,max=25)])
 	LastName= StringField('Last Name', validators=[DataRequired(),Length(min=2,max=25)])
-	location=StringField('Location',validators=[DataRequired()])
-	CurrentJob=StringField('Current Job')
+	location=StringField('City',validators=[DataRequired(),Length(min=2,max=10)])
+	CurrentJob=StringField('Title / Position')
 	Notworking=BooleanField('Open for Cases')
-	Linkedin=StringField('Linkedin')
+	Linkedin=StringField('Linkedin',validators=[URL()])
+	Facebook=StringField('Facebook',validators=[URL()])
+	Instagram=StringField('Instagram',validators=[URL()])
 	MinFee=IntegerField('Minimum Fee',validators=[DataRequired()])
 	MaxFee=IntegerField('Maximum Fee',validators=[DataRequired()])
 	FirstSpec=StringField('Specialization 1',validators=[DataRequired()])
 	SecSpec=StringField('Specialization 2')
 	ThirdSpec=StringField('Specialization 3')
 	Bio=StringField('Bio', validators=[DataRequired(),Length(min=2,max=200)])
-	country_code=IntegerField('Country Code',validators=[DataRequired()])
-	area_code= IntegerField('Area Code',validators=[DataRequired()])
-	number=IntegerField('Phone Number',validators=[DataRequired()])
-	profilephoto=FileField('Profile Photo')
+	
+	Educational_qualif_1 = StringField('Educational Qualification 1', validators=[DataRequired(),Length(min=2,max=200)])
+	From_1 = IntegerField('From',validators=[DataRequired(),Length(min=4,max=6)])
+	To_1 = IntegerField('To',validators=[DataRequired(),Length(min=4,max=6)])
+
+	Educational_qualif_2 = StringField('Educational Qualification 2', validators=[DataRequired(),Length(min=2,max=200)])
+	From_2 = IntegerField('From',validators=[DataRequired(),Length(min=4,max=6)])
+	To_2 = IntegerField('To',validators=[DataRequired(),Length(min=4,max=6)])
+
+	Educational_qualif_3 = StringField('Educational Qualification 3', validators=[DataRequired(),Length(min=2,max=200)])
+	From_3 = IntegerField('From',validators=[DataRequired(),Length(min=4,max=6)])
+	To_3 = IntegerField('To',validators=[DataRequired(),Length(min=4,max=6)])
+
+	Prof_qualif_1 = StringField('Proffesional Qualification 1', validators=[DataRequired(),Length(min=2,max=200)])
+	From_prof_1 = IntegerField('From',validators=[DataRequired(),Length(min=4,max=6)])
+	To_prof_1 = IntegerField('To',validators=[DataRequired(),Length(min=4,max=6)])
+
+	Prof_qualif_2 = StringField('Proffesional Qualification 2', validators=[DataRequired(),Length(min=2,max=200)])
+	From_prof_2 = IntegerField('From',validators=[DataRequired(),Length(min=4,max=6)])
+	To_prof_2 = IntegerField('To',validators=[DataRequired(),Length(min=4,max=6)])
+
+	Prof_qualif_3 = StringField('Proffesional Qualification 3', validators=[DataRequired(),Length(min=2,max=200)])
+	From_prof_3 = IntegerField('From',validators=[DataRequired(),Length(min=4,max=6)])
+	To_prof_3 = IntegerField('To',validators=[DataRequired(),Length(min=4,max=6)])
+
+	Number=IntegerField('Phone Number',validators=[DataRequired()])
+	Profilephoto=FileField('Profile Photo')
 	#Resume=FileField(u'Resume', [validators.regexp(u'^[^/\\]\.jpg$')])
-	submit=SubmitField('Create')
+	Submit=SubmitField()
 
 class LoginForm(FlaskForm):
 	email= StringField('Email', validators=[DataRequired(), Email()])
